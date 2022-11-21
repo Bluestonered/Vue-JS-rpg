16f473fe6b11961a79ed0d15c3a3919ff290a380
Bluestonred <foulhugo25620@gmail.com>
2022-11-21 10:55:28 +0100

    update Grid

--- src/views/TownsView.vue
+++ src/views/TownsView.vue
@@ -1,38 +1,76 @@
 <template>
   <v-container>
     <h1>Les Villes</h1>
+    <v-row no-gutters>
+      <v-col :cols="2">
+        <h3> <label for="filteractive">filtrage possible : </label><input type="checkbox" v-model="filterActive"
+            id="filteractive">
+          <div v-if="filterActive">
+            <label for="filtertown">filtre : </label><input v-model="filter" id="filtertown">
 
+            <ul>
+              <li v-for="(ville, index) in villesFiltre" :key="index">{{ ville.nom }}</li>
+            </ul>
+          </div>
+          <div v-else>
+            <ul>
+              <li v-for="(ville, index) in villes" :key="index">{{ ville.nom }}</li>
+            </ul>
 
-    <v-card class="text-h6 ma-10" outlined>
-      <v-row align="center" no-gutters>
-        <v-col cols:="12" sm="2" class="d-flex justify-center align-center text-center pa-5">
-          <h3> <label for="filteractive">filtrage possible : </label><input type="checkbox" v-model="filterActive"
-              id="filteractive">
-            <div v-if="filterActive">
-              <label for="filtertown">filtre : </label><input v-model="filter" id="filtertown">
+          </div>
+        </h3>
+      </v-col>
+      <v-divider inset vertical class="d-none d-sm-block"></v-divider>
+      <v-divider inset class="d-block d-sm-none"></v-divider>
+      <v-col :cols="10">
+        <h3>
+          <div v-if="villesFiltre.length === 1">
+            <v-row no-gutters>
+              <v-col order="last">
+                <v-card class="pa-2" outlined tile>
 
-              <ul>
-                <li v-for="(ville, index) in villesFiltre" :key="index">{{ ville.nom }}</li>
-              </ul>
-            </div>
-            <div v-else>
-              <ul>
-                <li v-for="(ville, index) in villes" :key="index">{{ ville.nom }}</li>
-              </ul>
+                  Boutique
+                </v-card>
+              </v-col>
+              <v-col>
+                <v-card class="pa-2" outlined tile v-for="(ville, index) in villesFiltre" :key="index">
+                  Rues: {{ ville.rues.length }}
+                </v-card>
+              </v-col>
+            </v-row>
+            <v-row no-gutters>
+              <v-col order="last">
+                <v-card class="pa-2" outlined tile>
 
-            </div>
-          </h3>
-        </v-col>
-        <v-divider inset vertical class="d-none d-sm-block"></v-divider>
-        <v-divider inset class="d-block d-sm-none"></v-divider>
-        <v-col cols:="12" sm="2" class="d-flex justify-center align-center text-center pa-5">
-          <h3>
-            <div v-if="villesFiltre.length === 1">Two</div>
-          </h3>
-        </v-col>
-      </v-row>
-    </v-card>
+                  Boutique 1
+                  Boutique 2
+                </v-card>
+              </v-col>
+              <v-col>
+                <v-card class="pa-2" outlined tile>
+                  Rue + Nbr Boutique
+                </v-card>
+              </v-col>
+            </v-row>
+            <v-row no-gutters>
+              <v-col order="last">
+                <v-card class="pa-2" outlined tile>
+                  Boutique 1
+                  Boutique 2
+                </v-card>
+              </v-col>
+              <v-col>
+                <v-card class="pa-2" outlined tile>
 
+                  Rue2 + Nbr Boutique
+                </v-card>
+              </v-col>
+            </v-row>
+
+          </div>
+        </h3>
+      </v-col>
+    </v-row>
   </v-container>
 
 
@@ -48,7 +86,6 @@ export default {
   data: () => ({
     filter: '',
     filterActive: false,
-    nbr: 0
   }),
   computed: {
     ...mapState(['villes']),
