36b7e482c956145b47c496e8aad838710ee39201
Bluestonered <foulhugo25620@gmail.com>
2022-11-29 15:35:13 +0100

    Update perso
    perso

--- src/views/PersosView.vue
+++ src/views/PersosView.vue
@@ -10,69 +10,68 @@
             <ul>
               <li v-for="(perso, index) in persoFiltre" :key="index">{{ perso.nom }}</li>
             </ul>
-            
+
           </h3>
-        <div v-if="persoFiltre.length === 1">
-          <v-row v-for="(perso, index) in persoFiltre" :key="index">
-          <v-col>
-            <v-row>
-              Attributs
+          <div v-if="persoFiltre.length === 1">
+
+            <v-row v-for="(perso, index) in persoFiltre" :key="index">
+              <v-col>
+                <v-row>
+                  Attributs
+                </v-row>
+                <v-row>
+                  Niveau :{{ perso.niveau }}
+                </v-row>
+                <v-row>
+                  Vie :{{ perso.attributs.vie }}
+                </v-row>
+                <v-row>
+                  Vitalité :{{ perso.attributs.vitalite }}
+                </v-row>
+                <v-row>
+                  Force :{{ perso.attributs.force }}
+                </v-row>
+                <v-row>
+                  Armure :{{ perso.attributs.protection }}
+                </v-row>
+                <v-row>
+                  Or :{{ perso.or }}
+                </v-row>
+              </v-col>
+
+              <v-col>
+                <v-row>
+                  emplacements
+                </v-row>
+                <v-row>
+
+                  <ul>
+                    <li v-for="(emplacement, index) in perso.emplacements" :key="index">
+                      {{ emplacement.nom }}
+                      <span v-if="emplacement.items.length !== 0">
+                        [{{ emplacement.items.length }}] : <span v-for="(item, indexItem) in emplacement.items"
+                          :key="indexItem">{{ item.nom }}, </span>
+                      </span>
+                    </li>
+                  </ul>
+
+                </v-row>
+                <v-row>
+                  item acheté :{{ perso.itemsAchetes.length }}
+                </v-row>
+              </v-col>
             </v-row>
-            <v-row>
-              Niveau :{{ perso.niveau }}
-            </v-row>
-            <v-row>
-              Vie :{{ perso.attributs.vie }}
-            </v-row>
-            <v-row>
-              Vitalité :{{ perso.attributs.vitalite }}
-            </v-row>
-            <v-row>
-              Force :{{ perso.attributs.force }}
-            </v-row>
-            <v-row>
-              Armure :{{ perso.attributs.protection }}
-            </v-row>
-            <v-row>
-              Or :{{ perso.or }}
-            </v-row>
-          </v-col>
-          
-          <v-col>
-            <v-row>
-              emplacements
-            </v-row>
-            <v-row v-for="(head,id) in perso.emplacements[0]" :key="id">
-              tête []:{{ head.length}}
-            </v-row>
-            <v-row>
-              Corps :{{ perso.nom }}
-            </v-row>
-            <v-row>
-              Mains :{{ perso.nom }}
-            </v-row>
-            <v-row>
-              Ceinture :{{ perso.nom }}
-            </v-row>
-            <v-row>
-              Sac a dos :{{ perso.nom }}
-            </v-row>
-            <v-row>
-              item acheté :{{ perso.nom }}
-            </v-row>
-          </v-col>
-        </v-row>
-        
-        
-        </div>
+
+
+          </div>
         </v-col>
 
-        
-      
+
+
 
 
       </v-row>
-      
+
 
 
     </v-app>
@@ -93,7 +92,7 @@ export default {
     ...mapState(['persos']),
     persoFiltre() {
       return this.persos.filter(v => v.nom.includes(this.filter))
-    }
+    },
   }
 
 }
