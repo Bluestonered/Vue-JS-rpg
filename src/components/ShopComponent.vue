1997926167a73702ff77e906c784f86baf743cfd
Bluestonered <foulhugo25620@gmail.com>
2022-12-05 17:26:10 +0100

    Update
    YEAH BOY

--- src/components/ShopComponent.vue
+++ src/components/ShopComponent.vue
@@ -13,44 +13,80 @@
         </v-row>
         <v-row>
             <v-col>
-                <CheckedList 
-                    :data="shop.itemStock" 
-                    :item-check="true"
-                    :checked="Array(shop.itemStock.length).fill(false)"
-                    :item-button="{ show: true, texte: 'acheter' }"
-                    :list-button="{ show: true, texte: 'tout acheter' }" 
-                    :fields="['nom', 'prix']" 
-                    @list-button-clicked="lButnClkd"
-                    @item-button-clicked="iButClkd" 
-                    @checked-changed="cChangd">
+                <CheckedList :data="shop.itemStock" :item-check="true"
+                    :checked="Array(shop.itemStock.length).fill(false)" :item-button="{ show: true, texte: 'acheter' }"
+                    :list-button="{ show: true, texte: 'tout acheter' }" :fields="['nom', 'prix']"
+                    @list-button-clicked="lButnClkd" @item-button-clicked="iButClkd" @checked-changed="cChangd">
                 </CheckedList>
             </v-col>
             <v-col>
-                <CheckedList 
-                    :data="shop.itemCommande" 
-                    :item-check="false"
-                    :item-button="{ show: true, texte: 'commande' }"
-                    :list-button="{ show: false}" 
-                    :fields="['nom', 'prix']" 
-                    @list-button-clicked="lButnClkd"
-                    @item-button-clicked="iButClkd" 
+                <CheckedList :data="shop.itemCommande" :item-check="false"
+                    :item-button="{ show: true, texte: 'commande' }" :list-button="{ show: false }"
+                    :fields="['nom', 'prix']" @list-button-clicked="lButnClkd" @item-button-clicked="iButClkd"
                     @checked-changed="cChangd">
                 </CheckedList>
             </v-col>
 
         </v-row>
+
+
+
+
+
+        <v-dialog
+        :value="dialog"
+        width="500"
+      >
+  
+        <v-card>
+          <v-card-title class="text-h5 grey lighten-2">
+            Privacy Policy
+          </v-card-title>
+  
+          <v-card-text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </v-card-text>
+  
+          <v-divider></v-divider>
+  
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="primary"
+              text
+              @click="dialog = false"
+            >
+              I accept
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
     </div>
+
+
 </template>
   
 <script>
 import CheckedList from '@/components/CheckedList.vue';
 export default {
     name: 'ShopComponent',
+    data: () => ({
+    dialog: false,
+  }),
     props: {
-        shop: Object
+        shop: Object,
+
     },
     components: {
         CheckedList
+    },
+    methods: {
+
+        iButClkd(id) {
+            this.dialog = true;
+            console.log(id);
+        }
     }
 }
 </script>
